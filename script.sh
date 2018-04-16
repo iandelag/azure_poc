028b3e96e8ab45b4427b063556c40b6c0baaa1f8
iandelag <37216586+iandelag@users.noreply.github.com>
2018-04-16 15:02:00 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /bin/echo "this is a test" > /tmp/test.txt
-curl -k puppetp4hm3tcsgzdku.canadacentral.cloudapp.azure.com:8140/packages/current/install.bash | sudo bash
+curl -k https://puppetp4hm3tcsgzdku.canadacentral.cloudapp.azure.com:8140/packages/current/install.bash | sudo bash
